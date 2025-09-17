ef46b44181db404235c52510b20ce1610f950b69
Thanadetch Lertwuthikarn <thanadetch.lert@gmail.com>
2025-09-17 21:11:34 +0700

    feat: add gRPC methods for creating and updating profiles in profiles module

--- apps/profiles/src/profiles.controller.ts
+++ apps/profiles/src/profiles.controller.ts
@@ -1,14 +1,30 @@
 import { Controller } from '@nestjs/common';
 import { ProfilesService } from './profiles.service';
 import { GrpcMethod } from '@nestjs/microservices';
-import { Profile } from '../generated/prisma/client';
+import { Profile, Prisma } from '../generated/prisma';
 
 @Controller()
 export class ProfilesController {
   constructor(private readonly profilesService: ProfilesService) {}
 
   @GrpcMethod('ProfilesService', 'GetProfiles')
-  GetProfiles(): Promise<{ results: Profile[] }> {
+  getProfiles(): Promise<{ results: Profile[] }> {
     return this.profilesService.getProfiles();
+  }
+
+  @GrpcMethod('ProfilesService', 'CreateProfile')
+  createProfile(data: Prisma.ProfileCreateInput): Promise<Profile> {
+    return this.profilesService.createProfile(data);
+  }
+
+  @GrpcMethod('ProfilesService', 'UpdateProfile')
+  updateProfile({
+    id,
+    data,
+  }: {
+    id: string;
+    data: Prisma.ProfileUpdateInput;
+  }): Promise<Profile> {
+    return this.profilesService.updateProfile(id, data);
   }
 }
--- apps/profiles/src/profiles.service.ts
+++ apps/profiles/src/profiles.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { Profile } from '../generated/prisma';
+import { Profile, Prisma } from '../generated/prisma';
 
 @Injectable()
 export class ProfilesService {
@@ -9,5 +9,21 @@ export class ProfilesService {
   async getProfiles(): Promise<{ results: Profile[] }> {
     const results = await this.prisma.profile.findMany();
     return { results };
+  }
+
+  async updateProfile(
+    id: string,
+    data: Prisma.ProfileUpdateInput,
+  ): Promise<Profile> {
+    return this.prisma.profile.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async createProfile(data: Prisma.ProfileCreateInput): Promise<Profile> {
+    return this.prisma.profile.create({
+      data,
+    });
   }
 }
