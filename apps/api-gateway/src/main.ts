02553ab352836ee757c051788df5733e187170c5
Thanadetch Lertwuthikarn <thanadetch.lert@gmail.com>
2025-10-05 02:43:08 +0700

    refactor: remove unused ConfigService import from main.ts

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
