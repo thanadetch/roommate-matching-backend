6d753be33be79383f84f4dfd4974865066705d45
mayysbst <mays6439@gmail.com>
2025-10-14 21:06:23 +0700

    feat(prisma): fix and update rooms schema

--- apps/roommate-matching/src/roommate-matching.module.ts
+++ apps/roommate-matching/src/roommate-matching.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { RoommateMatchingController } from './roommate-matching.controller';
 import { RoommateMatchingService } from './roommate-matching.service';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -10,6 +11,17 @@ import { PrismaService } from './prisma.service';
       isGlobal: true,
       envFilePath: './apps/roommate-matching/.env',
     }),
+    ClientsModule.register([
+      {
+        name: 'ROOMS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
+          queue: 'rooms_queue',
+          queueOptions: { durable: true },
+        },
+      },
+    ]),
   ],
   controllers: [RoommateMatchingController],
   providers: [RoommateMatchingService, PrismaService],
