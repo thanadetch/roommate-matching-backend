0082545bee4d7a3094d6e51030d6dfbf89d31568
Thanadetch Lertwuthikarn <thanadetch.lert@gmail.com>
2025-10-16 22:19:02 +0700

    feat(main): add global validation pipe to transform request data

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
